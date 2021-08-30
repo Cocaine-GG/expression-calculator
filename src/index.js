4990c30d93a97ee0d4431144786ae180903a5463
Daniel Kondratiuk <venga1.ua@gmail.com>
2021-08-31 01:05:40 +0200

    feat: implemented expression-calculator

--- src/index.js
+++ src/index.js
@@ -4,7 +4,19 @@ function eval() {
 }
 
 function expressionCalculator(expr) {
-    // write your solution here
+    expr = expr.replace(/ /g,'')
+    let brackets = 0
+    for (let i = 0; i < expr.length; i++) {
+        if (expr[i] === ')') brackets++
+        if (expr[i] === '(') brackets--
+    }
+    // expr.split('').forEach(el=>{
+    //     if (el === ')') brackets++
+    //     if (el === '(') brackets--
+    // })
+    if (expr.includes('/0')) throw Error('TypeError: Division by zero.')
+    if (brackets) throw Error('ExpressionError: Brackets must be paired')
+    return new Function(`return ${expr}`)()
 }
 
 module.exports = {
